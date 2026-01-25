54303200e08911ec2a29081bee55b9106451b6d2
Alan Lei <arkestaco@gmail.com>
2026-01-25 13:31:06 -0500

    Add The Zen of Python to 6_zen.py

---
+++ python/6_zen.py
@@ -0,0 +1,13 @@
+import this 
+
+"""
+
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+...
+
+"""
