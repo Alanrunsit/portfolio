f81c8e12978aa335c586c25c4eb6c537cf416f71
Alan Lei <arkestaco@gmail.com>
2026-02-05 23:07:43 -0500

    Update 7_create_gif.py

--- python/7_create_gif.py
+++ python/7_create_gif.py
@@ -1,1 +1,9 @@
+import imageio.v3 as iio 
 
+filenames = [ 'headshot1.png', 'headshot2.png']
+images = []
+
+for filename in filenames:
+    images.append(iio.imread(filename))
+
+iio.imwrite('headshots.gif', images, duration = 500, loop = 0)
